c9784fd96a46448b86125cb3b06b86c34cbd3442
George <donaldcruz040@gmail.com>
2025-07-29 18:57:32 +0100

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 function toggleMenu() {
-    const navList = document.querySelector("nav ul");
-    navList.classList.toggle("active");
-  }
-  
-  document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
+  const navList = document.querySelector("nav ul");
+  navList.classList.toggle("active");
+}
+
+
+document.getElementById('contact-form').addEventListener('submit', function () {
+  setTimeout(() => {
     alert("Thanks for reaching out! I'll get back to you soon.");
-    this.reset();
-  });
-  
+  }, 100);
+});
