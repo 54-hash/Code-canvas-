325d526a737922e862ac1b42e1ca846911ac096e
George <donaldcruz040@gmail.com>
2026-01-02 21:32:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-
 function toggleMenu() {
-  const navList = document.querySelector("nav ul");
-  navList.classList.toggle("active");
+  document.querySelector("nav ul").classList.toggle("active");
 }
 
-
-const fadeElements = document.querySelectorAll(".before-fade");
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add("fade-in");
-      entry.target.classList.remove("before-fade");
     }
   });
-}, {
-  threshold: 0.1
 });
 
-fadeElements.forEach(el => observer.observe(el));
+document.querySelectorAll(".before-fade").forEach(el => observer.observe(el));
